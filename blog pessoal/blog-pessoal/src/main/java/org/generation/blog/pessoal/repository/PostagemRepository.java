d96d5a7481f0128c57508640c573b3c571a21730
buchevitss <79327256+buchevitss@users.noreply.github.com>
2021-06-30 22:12:52 -0300

    Add files via upload

---
+++ blog pessoal/blog-pessoal/src/main/java/org/generation/blog/pessoal/repository/PostagemRepository.java
@@ -0,0 +1,12 @@
+package org.generation.blog.pessoal.repository;
+
+import java.util.List;
+
+import org.generation.blog.pessoal.model.Postagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+	public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+}
